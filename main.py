87f1e9a5a863104657b8cad4de59978f5f515c28
Xi Chen <xichen.u@gmail.com>
2021-04-02 08:48:28 -0400

    step 0: create a blank frame

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+BG_COLOR = 'SlateGray3'
+
+root = Tk()
+root.geometry('400x400')
+root.config(bg=BG_COLOR)
+root.resizable(0, 0)
+root.title("Smart Readathon Tracker")
+
+root.mainloop()
